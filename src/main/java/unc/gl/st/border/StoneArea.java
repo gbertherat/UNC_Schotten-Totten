8079e1ac39c2495d92f1fe06b62958bc5e9d7bb9
gbertherat <61923651+gbertherat@users.noreply.github.com>
2021-03-24 11:37:46 +1100

    feat(Border): Added methods and tests
    Added methods for classes Border, Stone and StoneArea
    Added tests for said methods

--- src/main/java/unc/gl/st/border/StoneArea.java
+++ src/main/java/unc/gl/st/border/StoneArea.java
@@ -1,5 +1,27 @@
 package unc.gl.st.border;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import unc.gl.st.card.ClanCard;
+
 public class StoneArea {
+    private static int MAX_NUM_CARDS = 3;
+    private List<ClanCard> cards;
+
+    public StoneArea(){
+        this.cards = new ArrayList<ClanCard>();
+    }
+
+    public boolean isFull(){
+        return this.cards.size() == MAX_NUM_CARDS;
+    }
     
+    public void addCard(ClanCard card){
+        this.cards.add(card);
+    }
+
+    public List<ClanCard> getCards(){
+        return this.cards;
+    }
 }

8a7b70bd9d3b5a83cf2cb10bb818231d843a4da2
gbertherat <61923651+gbertherat@users.noreply.github.com>
2021-04-29 09:39:12 +1100

    feat(View): Merged all view classes into one
    That allows to keep objects in memory, workaround to Vaadin not supporting POST requests at the moment.

--- src/main/java/unc/gl/st/MainView.java
+++ src/main/java/unc/gl/st/MainView.java
@@ -4,10 +4,21 @@ import com.vaadin.flow.component.HtmlContainer;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.Notification.Position;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+
+import unc.gl.st.game.Game;
+import unc.gl.st.game.GameOptions;
+import unc.gl.st.game.GameRegistry;
+import unc.gl.st.player.Player;
 
 @Route
 @Tag("st-main")
@@ -18,6 +29,7 @@ public class MainView extends HtmlContainer {
      *
      */
     private static final long serialVersionUID = 1L;
+    private VerticalLayout mainLayout;
 
     public VerticalLayout buildMainMenu(){
         VerticalLayout body = new VerticalLayout();
@@ -27,7 +39,7 @@ public class MainView extends HtmlContainer {
         Button jouer = new Button("JOUER"); // Bouton JOUER
         jouer.setClassName("mainButton");
         jouer.addClickListener(ev -> {
-            getUI().get().getPage().setLocation("start");
+            mainLayout.replace(body, buildPlayerMenu());
         });
 
         body.add(jouer);
@@ -38,8 +50,52 @@ public class MainView extends HtmlContainer {
         return body;
     }
 
+    public VerticalLayout buildPlayerMenu(){
+        VerticalLayout formLayout = new VerticalLayout();
+        formLayout.setSizeFull();
+        formLayout.setAlignItems(Alignment.CENTER);
+        formLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+    
+        TextField playerOneName = new TextField("Nom du joueur 1:");
+        playerOneName.setPrefixComponent(new Icon(VaadinIcon.USER));
+        formLayout.add(playerOneName);
+
+        TextField playerTwoName = new TextField("Nom du joueur 2:");
+        playerTwoName.setPrefixComponent(new Icon(VaadinIcon.USER));
+        formLayout.add(playerTwoName);
+
+        Button valider = new Button("Valider");
+        valider.setId("submitButton");
+
+        valider.addClickListener(ev -> {
+            if(playerOneName.getValue().length() == 0){
+                Notification.show("Erreur: Veuillez remplir le champ 'Nom du joueur 1'", 5000, Position.TOP_CENTER);
+                return;
+            }
+
+            if(playerTwoName.getValue().length() == 0){
+                Notification.show("Erreur: Veuillez remplir le champ 'Nom du joueur 2'", 5000, Position.TOP_CENTER);
+            }
+            Game game = GameRegistry.getInstance().createNewGame(new GameOptions(playerOneName+"&"+playerTwoName));
+            
+            game.addPlayer(new Player(playerOneName.getValue()));
+            game.addPlayer(new Player(playerTwoName.getValue()));
+
+            mainLayout.replace(formLayout, buildGame(game));
+        });
+
+        formLayout.add(valider);
+        return formLayout;
+    }
+
+    public VerticalLayout buildGame(Game game){
+        VerticalLayout gameLayout = new VerticalLayout();
+        
+        return gameLayout;
+    }
+
     public MainView(){
-        VerticalLayout mainLayout = new VerticalLayout();
+        mainLayout = new VerticalLayout();
         mainLayout.setClassName("main");
         mainLayout.setAlignItems(Alignment.CENTER);
 
